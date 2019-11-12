ce1ef33b64f6a7edc28253b691c7c821510f8d90
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-11-12 05:29:31 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,10 +13,11 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  
+  "SELECT SUM(tardies) FROM students;"
 end
 
 def average_gpa_for_9th_grade
+  "SELECT AVG(gpa) FROM "
 end
 
 ##
